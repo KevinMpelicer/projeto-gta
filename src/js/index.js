a0c8ed1fd97888b1d3a6913bac995e418625ca15
KevinMpelicer <magalhaesk97@gmail.com>
2024-01-28 17:51:48 -0300

    finalização do projeto

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {    
+    elementoPlataformas.classList.toggle("ativo");
+});
+
+
+
